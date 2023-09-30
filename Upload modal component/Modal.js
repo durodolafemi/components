dcbf058dcfdd57278d2dc679797fd377f769a6fc
Oluwafemi Durodola <106132361+durodolafemi@users.noreply.github.com>
2023-09-30 15:05:32 +0200

    Upload modal component - ready to use in any project

---
+++ Upload modal component/Modal.js
@@ -0,0 +1,145 @@
+import data from "./data.js";
+
+export default class Modal extends HTMLElement {
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+
+    this.uploadInprogress = data[0].uploadInprogress;
+    this.uploadFinished = data[0].uploadFinished;
+    this.uploadError = data[0].uploadError;
+
+    this.progressValue = 65;
+    this.status = "finished";
+
+    this.preconnectGstaticfontLink = document.createElement("link");
+    this.preconnectGstaticfontLink.rel = "preconnect";
+    this.preconnectGstaticfontLink.href = "https://fonts.gstatic.com";
+
+    this.preconnectfontLink = document.createElement("link");
+    this.preconnectfontLink.rel = "preconnect";
+    this.preconnectfontLink.href = "https://fonts.googleapis.com";
+
+    this.fontLink = document.createElement("link");
+    this.fontLink.href =
+      "https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap";
+    this.fontLink.rel = "stylesheet";
+
+    document.head.appendChild(this.preconnectfontLink);
+    document.head.appendChild(this.preconnectGstaticfontLink);
+    document.head.appendChild(this.fontLink);
+  }
+
+  async connectedCallback() {
+    await this.render();
+  }
+
+  static get observedAttributes() {
+    return ["status"];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === "status") {
+      this.status = newValue;
+      this.render();
+    }
+  }
+
+  async render() {
+    const extraSheet = new CSSStyleSheet();
+    extraSheet.replaceSync(await this.loadCSS());
+
+    this.shadow.adoptedStyleSheets = [extraSheet];
+
+    this.shadow.innerHTML = "";
+
+    this.shadow.innerHTML = `
+      <div class="modal-container">
+        <span class="status-icon-container">
+          ${this.renderIcon()}
+        </span>
+        
+        <span>
+          ${this.headerRender()}
+          ${this.bodyRender()}
+          <div class=${
+            this.status === "progress" ? "modal-footer" : "btn-container"
+          }>
+            ${this.footerRender()}
+          </div>
+        </span>
+        <button class="close-btn reset-this">X</button>
+      </div>
+    `;
+
+    this.shadow.querySelector(".close-btn").addEventListener("click", () => {
+      document.querySelector("modal-component-1").remove();
+    });
+  }
+
+  headerRender() {
+    if (this.status === "progress") {
+      return `<h3 class="modal-header">${this.uploadInprogress.header}</h3>`;
+    }
+    if (this.status === "error") {
+      return `<h3 class="modal-header">${this.uploadError.header}</h3>`;
+    }
+    if (this.status === "finished") {
+      return `<h3 class="modal-header">${this.uploadFinished.header}</h3>`;
+    }
+  }
+
+  async loadCSS() {
+    const fetchFile = await fetch("./Modal.css");
+    const fileContent = await fetchFile.text();
+
+    return fileContent;
+  }
+  renderIcon() {
+    if (this.status === "progress") {
+      return this.uploadInprogress.svgIcon;
+    }
+    if (this.status === "error") {
+      return this.uploadError.svgIcon;
+    }
+    if (this.status === "finished") {
+      return this.uploadFinished.svgIcon;
+    }
+  }
+
+  bodyRender() {
+    if (this.status === "progress") {
+      return `<p class="modal-body">${this.uploadInprogress.body}</p>`;
+    }
+    if (this.status === "error") {
+      return `<p class="modal-body">${this.uploadError.body}</p>`;
+    }
+    if (this.status === "finished") {
+      return `<p class="modal-body">${this.uploadFinished.body}</p>`;
+    }
+  }
+
+  footerRender() {
+    if (this.status === "progress") {
+      return `<span class="progress-container">
+                <progress class="progress-bar" max="100" value="65"></progress>
+                <span class="progress-value">${this.progressValue}%</span>
+              </span>
+              <button class="btn">Cancel</button>`;
+    }
+
+    if (this.status === "error") {
+      return `<button class="btn">Cancel</button>
+               <button class="btn">Retry</button>
+              `;
+    }
+
+    if (this.status === "finished") {
+      return `<button class="btn">Copy Link</button>
+               <button class="btn">Done</button>
+              `;
+    }
+  }
+}
+
+customElements.define("modal-component-1", Modal);
---
+++ Upload modal component/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    uploadInprogress: {
+      svgIcon: `<svg id="upload-inprogress" class="svg-icon" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M440-320h80v-168l64 64 56-56-160-160-160 160 56 56 64-64v168Zm40 240q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>`,
+      header: "Just a minute",
+      body: "Your file uploading right now. Just give us a second to finish your upload",
+    },
+
+    uploadFinished: {
+      svgIcon: `<svg id="upload-finished" class="svg-icon"  xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+            <path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
+          </svg>`,
+      header: "Your file was uploaded!",
+      body: "Your file was successfully uploaded. You can copy the link to your clipboard.",
+    },
+
+    uploadError: {
+      svgIcon: `<svg id="upload-error" class="svg-icon"  xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm-40-160h80v-240h-80v240Zm40 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>`,
+      header: "We are so sorry!",
+      body: "There was an error uploading your file. \nWould you like to try again?.",
+    },
+  },
+];
